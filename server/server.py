c5d3c98ce3867a4a38af4aa958cf0c0de1b2a320
Oli <o.radlett@gmail.com>
2022-12-02 21:20:27 +0000

    Initial work adding pgp functions

--- server/server.py
+++ server/server.py
@@ -5,31 +5,42 @@ import os
 import sys
 import distutils.spawn
 import ctypes
+import pickle
+
 
 class Server:
-    # Create socket
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except socket.error as err:
-        print ("socket creation failed with error %s" %(err))
-    
-    def __init__(self,ip,port):
+
+    def __init__(self, ip, port):
+        # Create socket
+        try:
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        except socket.error as err:
+            print("socket creation failed with error %s" % err)
         # Init socket params
-        self.s.bind((ip,port))
+        self.s.bind((ip, port))
         print("starting server on port " + str(port))
         self.gpg = self.initiate_gpg()
-        
 
     def start(self):
-        self.s.listen(5)  
+        self.s.listen(5)
         while True:
-             (conn, address) = self.s.accept()
-             data = conn.recv(1024)
-             if data:
+            (self.conn, self.address) = self.s.accept()
+            data = self.conn.recv(1024)
+            if data:
                 data = data.decode()
-                print(data)
-             time.sleep(1)
-             
+                if data[:4] == "list":
+                    self.list()
+                if data[:4] == "sign":
+                    self.sign()
+                if data[:6] == "verify":
+                    self.verify()
+                if data[:3] == "add":
+                    self.add_key(data[4:])
+            else:
+                print("Unexpected command %s" % data)
+
+            time.sleep(1)
+
     @staticmethod
     def initiate_gpg():
         if os.name == "nt":
@@ -45,18 +56,49 @@ class Server:
                 sys.exit("Error: gpg needs installing using your chosen package manager")
         return gpg
 
-    def sign_cert(self, cert):
+    def sign(self, cert):
         response = self.gpg.sign(cert)
         print(response)
         return response
 
+    def list(self):
+        public = self.gpg.list_keys()
+        private = self.gpg.list_keys(True)
+        numKeys = len(public) + len(private)
+        print("|----------------------------|")
+        print("| Keys: %d                    |" % numKeys)
+        print("|----------------------------|")
+        if numKeys > 0:
+            print("|  Type   |        ID        |")
+            print("|---------|------------------|")
+            for key in public:
+                print("| public  | %s |" % key["keyid"])
+                print("|----------------------------|")
+            for key in private:
+                print("| private | %s |" % key["keyid"])
+                print("|----------------------------|")
+
+        private = pickle.dumps(private)
+        public = pickle.dumps(public)
+        #Send(self.s).text("list private %s public %s" %(private, public))
+        self.conn.send("list private %s public %s".encode() %(private, public))
+
+    def verify(self):
+        print("unimplemented")
+    def add_key(self, data):
+        # temp
+        #self.gpg.import_keys_file(os.path.dirname(os.path.realpath(__file__)) + "/public_key.key")
+        #self.gpg.import_keys_file(os.path.dirname(os.path.realpath(__file__)) + "/private_key.key")
+        result = self.gpg.import_keys(data)
+        print("%s key(s) imported" %result.count)
+
+
 class Send:
-    def __init__(self,s: socket):
+    def __init__(self, s: socket):
         self.s = s
 
-    def text(self,text: str):
+    def text(self, text: str):
         self.s.send(text.encode())
-
 
 # class Listen:
 #     def __init__(self,s: socket):
