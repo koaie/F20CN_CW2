2627bf3024c560aa7c5ae87d535d3d1bc12f6880
Oli <o.radlett@gmail.com>
2022-12-02 18:32:35 +0000

    Fix git issues

--- server/main.py
+++ server/main.py
@@ -3,27 +3,3 @@ from server import Server
 server = Server('127.0.0.1', 8888)
 server.start()
 # server.sign_cert("test")
-
-
-    @staticmethod
-    def initiate_gpg():
-        if os.name == "nt":
-            path = os.path.dirname(os.path.realpath(__file__)) + "\\bin\\gpg.exe"
-            if os.path.exists(path):
-                gpg = gnupg.GPG(gpgbinary=path)
-            else:
-                sys.exit("Error: gpg windows binary is missing from bin folder")
-        else:
-            if distutils.spawn.find_executable("gpg"):
-                gpg = gnupg.GPG()
-            else:
-                sys.exit("Error: gpg needs installing using your chosen package manager")
-        return gpg
-
-    def sign_cert(self, cert):
-        response = self.gpg.sign(cert)
-        print(response)
-        return response
-
-test = Server()
-test.sign_cert("test")
